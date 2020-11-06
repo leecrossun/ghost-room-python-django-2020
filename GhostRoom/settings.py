46ab7a6c953a4a9e87e946f8ad52ad5d920eb70a
hyosunLee <leecrossun@gmail.com>
2020-11-06 16:47:52 +0900

    heroku_files

--- GhostRoom/settings.py
+++ GhostRoom/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +130,8 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn GhostRoom.wsgi --log-file -
